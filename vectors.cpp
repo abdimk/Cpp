315b4b47603a76e118be5208e09d87af955964b3
Abdimk <63399762+abdimk@users.noreply.github.com>
2022-10-11 21:04:53 +0300

    updated

--- vectors.cpp
+++ vectors.cpp
@@ -4,34 +4,49 @@
 using namespace std;
 
 int main(){
-  vector <int> vector1{};
-  vector <int> vector2{};
+   vector <int> vector1;
+   vector <int> vector2;
 
-  vector1.push_back(10);
-  vector1.push_back(20);
+   vector1.push_back(10);
+   vector1.push_back(20);
 
-  cout << "First element in vector1 one: "<< vector1.at(0)<<endl;
-  cout << "Second element in vector1 two: "<< vector1.at(1)<<endl;
-  cout << "And the total number of elemnts is "<< vector1.size()<<endl;
+   cout <<"++++++===vector1===++++++\n";
+   cout <<vector1.at(0)<<endl;
+   cout <<vector1.at(1)<<endl;
+   cout <<"The size of the vector1 is "<<vector1.size()<<endl;
 
-  vector2.push_back(100);
-  vector2.push_back(200);
-
-  cout << "First element in vector2 : "<< vector2.at(0)<<endl;
-  cout << "Second element in vector2 : "<< vector2.at(1)<<endl;
-  cout << "And the total number of elemnts is "<< vector2.size()<<endl;
-
-  vector <vector<int>> vector2d {
-    {0},
-    {0},
-    {0},
-    {0},
-  };
-  vector2d.push_back(vector1);
-  vector2d.push_back(vector2);
-  cout << vector2d.size() <<endl;
+   vector2.push_back(100);
+   vector2.push_back(200);
+   
+   cout <<"++++++===vector2===++++++\n";
+   cout <<vector2.at(0)<<endl;
+   cout <<vector2.at(1)<<endl;
+   cout <<"The size of the vector2 is "<<vector2.size()<<endl;
 
 
-  return 0;
+
+   vector <vector<int>> vector_2d;
+   vector_2d.push_back(vector1);
+   vector_2d.push_back(vector2);
+
+
+   cout << "++++++++===vector_2d===+++++++++\n";
+   cout << vector_2d.at(0).at(0)<<endl;
+   cout << vector_2d.at(0).at(1)<<endl;
+   cout << vector_2d.at(1).at(0)<<endl;
+   cout << vector_2d.at(1).at(1)<<endl;
+   cout << "++++++++===vector_2d===+++++++++";
+   
+   vector1.at(0) = 1000;
+
+   cout << "\n"<<vector1.at(0)<<endl;
+
+   
+
+
+
+   return 0;
+
+
+  
 }
-
