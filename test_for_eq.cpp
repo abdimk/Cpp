2ad5021d3d41da8d07ad9c3e0af773ef9c89317e
abdimk <abdisamk@gmail.com>
2022-10-17 18:17:01 +0300

    bb

--- test_for_eq.cpp
+++ test_for_eq.cpp
@@ -11,7 +11,7 @@ int main(){
     cin >> num1 >> num2;
     equal_result = (num1 == num2);
     not_equal_result = (num1 != num2);
-    
-    cout << "equal result"<< equal_result<<endl;
-    cout << "not equal result"<< not_equal_result;
+
+    cout << "equal result: "<< equal_result<<endl;
+    cout << "not equal result: "<< not_equal_result<<endl;
 }
