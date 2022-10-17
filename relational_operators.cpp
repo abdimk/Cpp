8669ecc794c143f2a9958feeb9a82882b82f3324
abdimk <abdisamk@gmail.com>
2022-10-17 19:10:52 +0300

    commit

--- relational_operators.cpp
+++ relational_operators.cpp
@@ -7,13 +7,16 @@ int main(){
     const int upper_limit {10};
     const int lower_limit {20};
     int num1{},num2;
+    cout<< boolalpha;
 
     cout << "Enter an integer that is greater than "<<upper_limit<<":"<<endl;
     cin >> num1;
-    cout<< boolalpha;
     cout << num1 <<" > "<<upper_limit<<"is"<<(num1>upper_limit)<<endl;
     cout << "Enter an integer that is less than or equal to "<<upper_limit<<":"<<endl;
     cin >> num2;
+    cout << num1 <<"<="<<upper_limit<<"is"<<(num1<=upper_limit)<<endl;
+
+
 
     
 
