6b70fb7e84310de2a9ea42df653b4a18b6b684f4
AbdiMK <63399762+abdimk@users.noreply.github.com>
2022-10-23 11:33:10 +0300

    some examples of while loop

---
+++ while_cod.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ /*
+    int i{};
+
+    while(i <= 10){
+        if(i % 2 == 0)
+            cout<<i<<endl;
+        ++i;
+    }
+
+    int scores [] {100, 90, 87};
+    int array_size{3};
+    int i{0};
+
+    while(i < array_size){
+        cout<<scores[i]<<endl;
+        ++i;
+    }
+
+
+    int number{};
+
+    string message{"Enter an integer less than 100:"};
+
+    cout <<message;
+    cin >> number;
+
+    while(number <=100){
+        cout <<message;
+        cin >>number;
+    }
+
+    
+    int number{};
+    string message{"Enter a number between 1 and 5:"};
+
+    cout <<message;
+    cin >> number ;
+
+    while(number <= 1 || number >= 5 ){
+        cout <<message;
+        cin >>number;
+    }
+    cout <<"Thanks!"<<endl;
+
+*/
+
+}
+
+
+
+
+
+
+
